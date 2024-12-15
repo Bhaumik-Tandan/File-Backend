a61bdf6752cec2f91834ffb06ca5249618e91243
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-12-15 13:24:29 +0530

    [add] basic code setup

---
+++ src/app.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { FileModule } from './file/file.module';
+@Module({
+  imports: [
+    ConfigModule.forRoot({ envFilePath: ['.env'], isGlobal: true }),
+    MongooseModule.forRoot(process.env.DATABASE_URL),
+    FileModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
